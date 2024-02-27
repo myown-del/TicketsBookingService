c14d256216a23c2dfb6070e264bd477783fd42ce
Onulonus <onulonus@gmail.com>
2024-02-27 09:18:42 +0300

    add gitignore

--- src/Infrastructure/TicketsBooking.Infrastructure.Persistence/obj/Debug/net8.0/TicketsBooking.Infrastructure.Persistence.AssemblyInfo.cs
+++ src/Infrastructure/TicketsBooking.Infrastructure.Persistence/obj/Debug/net8.0/TicketsBooking.Infrastructure.Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketsBooking.Infrastructure.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a68263872a034a39eba560487273c0118532b477")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketsBooking.Infrastructure.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketsBooking.Infrastructure.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
