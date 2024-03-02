f0ad88a1fce2cbec3e2ba028ad62cc1ac76c7308
DmitryKosenkov <110896248+DmitryKosenkov@users.noreply.github.com>
2024-03-02 18:37:55 +0300

    Update DBStructure.sql

--- DBStructure.sql
+++ DBStructure.sql
@@ -1,4 +1,4 @@
-﻿do
+do
 $$
 begin
     create schema if not exists bt;
@@ -29,76 +29,70 @@ begin
     )
     without oids ;
 
-
-
-
-
-
     create table if not exists bt.theaters(
-            thtr_id bigint default nextval('bt.thtr_seq'::regclass),
-            thtr_name text not null,
-            thtr_adress text not null,
-            thtr_type char(1) not null ,    
-            --
-            constraint thtr_pk primary key (thtr_id),
-            constraint thtr_type_check
-                check (thtr_type in ('t', 'm'))                                         
-        )
-        without oids ;
+        thtr_id bigint default nextval('bt.thtr_seq'::regclass),
+        thtr_name text not null,
+        thtr_adress text not null,
+        thtr_type char(1) not null ,    
+        --
+        constraint thtr_pk primary key (thtr_id),
+        constraint thtr_type_check
+            check (thtr_type in ('t', 'm'))                                         
+    )
+    without oids ;
     
-        create table if not exists bt.halls(
-                hall_id bigint default nextval('bt.hall_seq'::regclass),
-                hall_thtr_id bigint not null,
-                hall_number text not null,    
-                --
-                constraint hall_pk primary key (hall_id)                                    
-            )
-            without oids ;
+    create table if not exists bt.halls(
+        hall_id bigint default nextval('bt.hall_seq'::regclass),
+        hall_thtr_id bigint not null,
+        hall_number text not null,    
+        --
+        constraint hall_pk primary key (hall_id)                                    
+    )
+    without oids ;
     
     create table if not exists bt.users(
-                    user_id bigint default nextval('bt.user_seq'::regclass),
-                    user_login text not null,
-                    user_passworsd_hach text not null,  
-                    user_is_admin boolean not null,  
-                    --
-                    constraint user_pk primary key (user_id)                                    
-                )
-                without oids ;
+        user_id bigint default nextval('bt.user_seq'::regclass),
+        user_login text not null,
+        user_passworsd_hach text not null,  
+        user_is_admin boolean not null,  
+        --
+        constraint user_pk primary key (user_id)                                    
+    )
+    without oids;
 
     create table if not exists bt.sessions(
-                                           sess_id bigint default nextval('bt.sess_seq'::regclass),
-                                           sess_act_id bigint not null,
-                                           sess_hall_id bigint not null,
-                                           sess_date timestamp not null,
-        --
-                                           constraint sess_pk primary key (sess_id)
+       sess_id bigint default nextval('bt.sess_seq'::regclass),
+       sess_act_id bigint not null,
+       sess_hall_id bigint not null,
+       sess_date timestamp not null,
+       --
+       constraint sess_pk primary key (sess_id)
     )
-        without oids ;
+    without oids ;
 
 
     create table if not exists bt.tickets(
-                                              tick_id bigint default nextval('bt.tick_seq'::regclass),
-                                              tick_seat_id bigint not null,
-                                              tick_sess_id bigint not null,
-                                              tick_user_id bigint not null,
+        tick_id bigint default nextval('bt.tick_seq'::regclass),
+        tick_seat_id bigint not null,
+        tick_sess_id bigint not null,
+        tick_user_id bigint not null,
         --
-                                              constraint tick_pk primary key (tick_id),
-                                              constraint tick_uk_01 unique (tick_sess_id, tick_seat_id)
-                                    
+        constraint tick_pk primary key (tick_id),
+        constraint tick_uk_01 unique (tick_sess_id, tick_seat_id)
+
     )
-        without oids ;
+    without oids ;
 
 
     create table if not exists bt.seats(
-                                             seat_id bigint default nextval('bt.seat_seq'::regclass),
-                                             seat_hall_id bigint not null,
-                                             seat_place bigint not null,
-                                             seat_row bigint not null,
+        seat_id bigint default nextval('bt.seat_seq'::regclass),
+        seat_hall_id bigint not null,
+        seat_place bigint not null,
+        seat_row bigint not null,
         --
-                                             constraint seat_pk primary key (seat_id)
-
+        constraint seat_pk primary key (seat_id)
     )
-        without oids ;
+    without oids;
     
     begin alter table bt.actions add constraint act_hall_fk foreign key (act_hall_id) references bt.halls(hall_id); exception when duplicate_object then null; end;
 
