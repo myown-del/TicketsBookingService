9e8880c7445ed534b1d8dca903048ca11fda1f69
Onulonus <onulonus@gmail.com>
2024-03-04 23:24:09 +0300

    fix IUserService methods

--- src/Application/TicketsBooking.Application.Abstractions/Persistence/Repositories/IUserService.cs
+++ src/Application/TicketsBooking.Application.Abstractions/Persistence/Repositories/IUserService.cs
@@ -4,9 +4,9 @@ namespace TicketsBooking.Application.Abstractions.Persistence.Repositories;
 
 public interface IUserService
 {
-    public JwtTokenDto Registration(string phoneNumber, string password, string? name = null, string? email = null, DateTime? birthdayDate = null);
+    public JwtTokenDto RegisterUser(string phoneNumber, string password, string? name = null, string? email = null, DateTime? birthdayDate = null);
 
-    public JwtTokenDto Authorization(string phoneNumber, string password);
+    public JwtTokenDto AuthorizeUser(string phoneNumber, string password);
 
     public JwtTokenDto RefreshToken(string refreshToken);
 }
