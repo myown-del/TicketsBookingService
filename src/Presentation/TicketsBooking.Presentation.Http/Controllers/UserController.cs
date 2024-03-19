b2e06213afdc410ed5a18f42cd7d25ced60406c7
onulonus <onulonus@gmail.com>
2024-03-20 01:44:03 +0300

    divide User into 2 services: Authorization and User

---
+++ src/Presentation/TicketsBooking.Presentation.Http/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TicketsBooking.Presentation.Http.Controllers;
+
+
+[ApiController]
+[Route("api/users")]
+public class UserController : ControllerBase
+{
+    
+}
