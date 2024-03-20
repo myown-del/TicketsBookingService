263a99e031c9b47d844395f7b052887314acfa08
Maria <Mary.Volzheva@gmail.com>
2024-03-20 15:07:01 +0300

    errors

--- src/Application/TicketsBooking.Application.Models/Entities/User.cs
+++ src/Application/TicketsBooking.Application.Models/Entities/User.cs
@@ -2,17 +2,35 @@
 
 public class User
 {
+    public User(
+        long id,
+        string name,
+        string email,
+        DateTime birthdayDate,
+        string login,
+        string passwordHash,
+        bool isAdmin)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+        BirthdayDate = birthdayDate;
+        Login = login;
+        PasswordHash = passwordHash;
+        IsAdmin = isAdmin;
+    }
+
     public long Id { get; set; }
 
-    public string? Name { get; set; }
+    public string Name { get; set; }
 
-    public string? Email { get; set; }
+    public string Email { get; set; }
 
     public DateTime BirthdayDate { get; set; }
 
-    public string? Login { get; set; }
+    public string Login { get; set; }
 
-    public string? PasswordHash { get; set; }
+    public string PasswordHash { get; set; }
 
     public bool IsAdmin { get; set; }
 }
